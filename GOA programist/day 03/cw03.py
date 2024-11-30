066addc9655e3b8c47d2afbfed1f75ec24f136a4
nikaeliashvili77 <nikushaeliashvili208@gmail.com>
2024-11-30 22:15:29 +0400

    Add files via upload

---
+++ GOA programist/day 03/cw03.py
@@ -0,0 +1,12 @@
+# ცვლადების შექმნა
+name = "ნიკა"
+surname = "ელიაშვილი"
+age = 14
+animal = "dog"
+
+
+#ცვლადის დაპრინტვა
+print("name:" "ნიკა" )
+print("surname:" "ელიაშვილი" )
+print("age:" "14" )
+print("animal:" "ძაღლი" )
---
+++ GOA programist/day 03/hw03.py
@@ -0,0 +1,13 @@
+# ცვლადების შექმნა
+name = "ნიკა"
+surname = "ელიაშვილი"
+age = 14
+hobby = "ფეხბურთი "
+country = "საქართველო"
+
+#ცვლადის დაპრინტვა
+print("name:" "ნიკა" )
+print("surname:" "ელიაშვილი" )
+print("age:" "14" )
+print("hobby:" "ფეხბურთი" )
+print("country:" "საქართველო" )
